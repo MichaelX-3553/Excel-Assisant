93bae81e4b58505c625cf9005413b984537c6669
MichaelX <100671608+MichaelX-3553@users.noreply.github.com>
2023-01-01 16:11:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Add a new user to the table
 function addToTable() {
 	// Get the form input values
-	var name = document.getElementById('name').value;
-	var email = document.getElementById('email').value;
-	var country = document.getElementById('country').value;
-	var hasProof = document.getElementById('has-proof').checked;
-	var facebookLink = document.getElementById('facebook-link').value;
+	let name = document.getElementById('name').value;
+	let email = document.getElementById('email').value;
+	let country = document.getElementById('country').value;
+	let hasProof = document.getElementById('has-proof').checked;
+	let facebookLink = document.getElementById('facebook-link').value;
 
 	// Check if the user already exists in the table
-	var userExists = false;
-	var rows = document.getElementById('user-table').rows;
-	for (var i = 1; i < rows.length; i++) {
+	let userExists = false;
+	let rows = document.getElementById('user-table').rows;
+	for (let i = 1; i < rows.length; i++) {
 		if (rows[i].cells[1].innerHTML == email) {
 			userExists = true;
 			break;
@@ -20,30 +20,37 @@ function addToTable() {
 	// If the user does not already exist, add them to the table
 	if (!userExists) {
 		// Create a new row in the table
-		var table = document.getElementById('user-table');
-		var row = table.insertRow(-1);
+		let table = document.getElementById('user-table');
+		let row = table.insertRow(-1);
 
 		// Add the user information to the cells of the new row
-		var cell1 = row.insertCell(0);
-		var cell2 = row.insertCell(1);
-		var cell3 = row.insertCell(2);
-		var cell4 = row.insertCell(3);
-		var cell5 = row.insertCell(4);
+		let cell1 = row.insertCell(0);
+		let cell2 = row.insertCell(1);
+		let cell3 = row.insertCell(2);
+		let cell4 = row.insertCell(3);
+		let cell5 = row.insertCell(4);
+		let cell6 = row.insertCell(5);
 		cell1.innerHTML = name;
 		cell2.innerHTML = email;
 		cell3.innerHTML = country;
 		cell4.innerHTML = hasProof;
 		cell5.innerHTML = facebookLink;
+		cell6.innerHTML = deleteButton();
+
+		cell6.childNodes[0].addEventListener('click', () => {
+			cell6.childNodes[0].parentElement.parentElement.remove();
+			console.log('hello parent');
+		});
 	}
 }
 
 // Download the table data as an Excel file
 function downloadExcel() {
 	// Convert the table data to an array of objects
-	var data = [];
-	var rows = document.getElementById('user-table').rows;
-	for (var i = 1; i < rows.length; i++) {
-		var rowData = {};
+	let data = [];
+	let rows = document.getElementById('user-table').rows;
+	for (let i = 1; i < rows.length; i++) {
+		let rowData = {};
 		rowData.name = rows[i].cells[0].innerHTML;
 		rowData.email = rows[i].cells[1].innerHTML;
 		rowData.country = rows[i].cells[2].innerHTML;
@@ -53,10 +60,10 @@ function downloadExcel() {
 	}
 
 	// Use Papa Parse to convert the data to a CSV string
-	var csv = Papa.unparse(data);
+	let csv = Papa.unparse(data);
 
 	// Create a hidden link element and click it to trigger the download
-	var link = document.createElement('a');
+	let link = document.createElement('a');
 	link.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
 	link.download = 'users.csv';
 	link.style.display = 'none';
@@ -72,13 +79,13 @@ function readExcel(file) {
 		header: true,
 		complete: function (results) {
 			// Loop through the data and add each user to the table
-			var data = results.data;
-			for (var i = 0; i < data.length; i++) {
-				var name = data[i].name;
-				var email = data[i].email;
-				var country = data[i].country;
-				var hasProof = data[i].hasProof;
-				var facebookLink = data[i].facebookLink;
+			let data = results.data;
+			for (let i = 0; i < data.length; i++) {
+				let name = data[i].name;
+				let email = data[i].email;
+				let country = data[i].country;
+				let hasProof = data[i].hasProof;
+				let facebookLink = data[i].facebookLink;
 				addUser(name, email, country, hasProof, facebookLink);
 			}
 		},
@@ -88,9 +95,9 @@ function readExcel(file) {
 // Add a user to the table
 function addUser(name, email, country, hasProof, facebookLink) {
 	// Check if the user already exists in the table
-	var userExists = false;
-	var rows = document.getElementById('user-table').rows;
-	for (var i = 1; i < rows.length; i++) {
+	let userExists = false;
+	let rows = document.getElementById('user-table').rows;
+	for (let i = 1; i < rows.length; i++) {
 		if (rows[i].cells[1].innerHTML == email) {
 			userExists = true;
 			break;
@@ -100,21 +107,34 @@ function addUser(name, email, country, hasProof, facebookLink) {
 	// If the user does not already exist, add them to the table
 	if (!userExists) {
 		// Create a new row in the table
-		var table = document.getElementById('user-table');
-		var row = table.insertRow(-1);
+		let table = document.getElementById('user-table');
+		let row = table.insertRow(-1);
 
 		// Add the user information to the cells of the new row
-		var cell1 = row.insertCell(0);
-		var cell2 = row.insertCell(1);
-		var cell3 = row.insertCell(2);
-		var cell4 = row.insertCell(3);
-		var cell5 = row.insertCell(4);
+		let cell1 = row.insertCell(0);
+		let cell2 = row.insertCell(1);
+		let cell3 = row.insertCell(2);
+		let cell4 = row.insertCell(3);
+		let cell5 = row.insertCell(4);
+		let cell6 = row.insertCell(5);
 		cell1.innerHTML = name;
 		cell2.innerHTML = email;
 		cell3.innerHTML = country;
 		cell4.innerHTML = hasProof;
 		cell5.innerHTML = facebookLink;
+		cell6.innerHTML = deleteButton();
+
+		cell6.childNodes[0].addEventListener('click', () => {
+			cell6.childNodes[0].parentElement.parentElement.remove();
+			console.log('hello parent');
+		});
 	}
 }
 
-// 925RF0L5 MTN-MobileWIFI-E5573
+function deleteButton() {
+	let button = document.createElement('button');
+	button.classList.add('delete-btn');
+	button.textContent = 'Delete';
+
+	return button.outerHTML;
+}
